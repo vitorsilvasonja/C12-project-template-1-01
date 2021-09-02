3e8661ffae1481de150420dd48c5218b9a9fea08
frs-marcelle <89610805+frs-marcelle@users.noreply.github.com>
2021-09-02 17:59:03 -0300

    Add files via upload

---
+++ C12-project-template-main/sketch.js
@@ -0,0 +1,48 @@
+var path,boy, leftBoundary,rightBoundary;
+var pathImg,boyImg;
+var i;
+
+function preload(){
+  //loadImage (carregarImagem) da pista)
+  //loadAnimation (carregarAnimação) de corrida para o menino
+ 
+}
+
+function setup(){
+  
+  createCanvas(400,400);
+ //crie um sprite para a pista 
+//adicione uma imagem para a pista
+//Faça com que a pista seja um fundo que se move dando a ela velocity Y.
+path.scale=1.2;
+
+//crie um sprite de menino
+//adicione uma animação de corrida para ele
+boy.scale=0.08;
+  
+//crie um limite à esquerda
+leftBoundary=createSprite(0,0,100,800);
+//defina visibilidade como falsa para o limite à esquerda
+
+//crie um limite à direita
+rightBoundary=createSprite(410,0,100,800);
+//defina visibilidade como falsa para o limite à direita
+}
+
+function draw() {
+  background(0);
+  path.velocityY = 4;
+  
+  // mover o menino com o mouse usando mouseX
+  
+  edges= createEdgeSprites();
+  boy.collide(edges[3]);
+  // colidir o menino com os limites invisíveis da esquerda e da direita
+  
+  //código para redefinir o fundo
+  if(path.y > 400 ){
+    path.y = height/2;
+  }
+  
+  drawSprites();
+}
